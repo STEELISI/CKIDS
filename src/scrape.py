73a3a1634a541cc6660469f3e6aab85cda0802f5
Hardik Mahipal Surana <hardiksurana01@gmail.com>
2020-02-14 12:26:02 -0800

    added requirements and additional comments

--- src/scrape.py
+++ src/scrape.py
@@ -8,12 +8,12 @@ API_ROOT = config['ZENODO_API']['API_ROOT']
 ACCESS_KEY = config['ZENODO_API']['ACCESS_TOKEN']
 params = {
     'page': '1',
-    'size': '20',
-    'q': 'cybersecurity',
+    'size': '2000',             # set max. number of search results
+    'q': 'cybersecurity',       # add vocabulary keywords here
     'access_token': ACCESS_KEY
 }
 
-res = requests.get(url=API_ROOT + 'records/?page=1&size=20&q=cybersecurity', params=params)
+res = requests.get(url=API_ROOT + 'records/', params=params)
 
 with open('result.json', 'w') as f:
     data = res.json()
