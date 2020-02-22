9857b42cb6a4115441ae29b99d7e9f32fdae6fa3
Hardik Mahipal Surana <hardiksurana01@gmail.com>
2020-02-22 11:24:46 -0800

    added documentation and error handling

--- README.md
+++ README.md
@@ -1,12 +1,45 @@
 # CKIDS
-Project for creating a knowledge graph of cybersecurity artifacts mined from Zenodo
+
+## Project Description
+**Goal**: To create a knowledge graph of cybersecurity artifacts mined from [Zenodo](https://www.zenodo.org)
 
 Jelena's group has been working on collecting resources/artifacts that are important for cybersecurity. These resources are papers, datasets or software that are related to a cybersecurity topic. Jelena's student has been compiling the table she sent in a manual manner. We would like to speed up the process by identifying which resources in Zenodo are relevant to cybersecurity, their description and how do they relate together and build a knowledge graph allowing Jelena’s group to efficiently search for related artifacts.
 
-## Artefacts Format
+## Data Collection
+To collect data from Zenodo, we use the Zenodo API. The file ```scrape.py``` sends GET requests to the Zenodo API and stores the results as specified.
 
-The use case continues to be to retrieve Zenodo artifacts related to cybersecurity.
+Required command-line arguments:
+```shell script
+python3 src/scrape.py -h
+usage: scrape.py [-h] -k KEYWORD [KEYWORD ...] -s SIZE [-db]
 
+optional arguments:
+  -h, --help            show this help message and exit
+  -k KEYWORD [KEYWORD ...], --keyword KEYWORD [KEYWORD ...]
+                        keyword(s) to be searched in Zenodo
+  -s SIZE, --size SIZE  number of query results to be requested from Zenodo
+  -db, --db             (optional) stores collected data to MongoDB in the AWS
+                        EC2 instance
+```
+
+Execution Examples:
+```shell script
+cd <PROJECT_ROOT>
+
+# query with a single keyword, store results in JSON file
+python3 src/scrape.py -k cybersecurity -s 100
+
+# query with multiple keywords, store results in JSON file
+python3 src/scrape.py -k cybersecurity vulnerability -s 100
+
+# store results in MongoDB in the AWS EC2 instance
+python3 src/scrape.py -k cybersecurity -s 100 -db
+```
+
+Expected Result: JSON document containing data for each artifact.
+A sample of an artifact collected from Zenodo can be found in ```results/sample_artifact.json```
+
+## Artifacts Storage Schema
 When mining artifacts, the following information is saved:
 
 * id: Zenodo URL/DOI.
--- src/scrape.py
+++ src/scrape.py
@@ -1,20 +1,74 @@
-import requests
+import argparse
 from configparser import ConfigParser
 import json
+import os
+import requests
 
 config = ConfigParser()
-config.read('secrets.ini')
-API_ROOT = config['ZENODO_API']['API_ROOT']
-ACCESS_KEY = config['ZENODO_API']['ACCESS_TOKEN']
-params = {
-    'page': '1',
-    'size': '2000',             # set max. number of search results
-    'q': 'cybersecurity',       # add vocabulary keywords here
-    'access_token': ACCESS_KEY
-}
+pardir = os.getcwd()
+config.read(os.path.join(pardir, 'resources/secrets.ini'))
 
-res = requests.get(url=API_ROOT + 'records/', params=params)
 
-with open('result.json', 'w') as f:
-    data = res.json()
-    f.write(json.dumps(data, indent=4))
+def get_artifacts_zenodo(keyword, size):
+    """
+    sends a GET request to Zenodo and retrieves artifacts based on search keywords and number of results
+
+    :param keyword: List
+        A list of strings to search based on (vocabulary)
+    :param size: str
+        A number entered as a string to request a fixed number of search results
+    :return: dict
+        a JSON document with the response data or None
+    """
+    print(config.sections())
+    API_ROOT = config['ZENODO_API']['API_ROOT']
+    ACCESS_KEY = config['ZENODO_API']['ACCESS_TOKEN']
+
+    params = {
+        'page': '1',
+        'q': keyword,
+        'size': size,
+        'access_token': ACCESS_KEY
+    }
+
+    try:
+        res = requests.get(url=API_ROOT + 'records/', params=params)
+    except requests.exceptions.ConnectionError as conn_err:
+        print(conn_err)
+        return None
+    except requests.exceptions.HTTPError as http_err:
+        print(http_err)
+        return None
+
+    return res.json()
+
+
+def main():
+    # read command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-k", "--keyword",
+                        nargs='+',
+                        help="keyword(s) to be searched in Zenodo",
+                        required=True)
+    parser.add_argument("-s", "--size",
+                        help='number of query results to be requested from Zenodo',
+                        required=True)
+    parser.add_argument("-db", "--db",
+                        help="(optional) stores collected data to MongoDB in the AWS EC2 instance",
+                        action='store_true')
+    args = parser.parse_args()
+
+    # get artifacts from Zenodo
+    data = get_artifacts_zenodo(args.keyword, args.size)
+
+    # write results into a database
+    if args.db:
+        pass
+    # write results into a JSON file
+    else:
+        with open(os.path.join(pardir, 'results/zenodo_artifacts_dump.json'), 'w') as f_ptr:
+            f_ptr.write(json.dumps(data, indent=4))
+
+
+if __name__ == '__main__':
+    main()
