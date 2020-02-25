554561a8acb6cdb35a2c0075db8bc5d59fd8cb61
Tianxin <zhoutx0@gmail.com>
2020-02-24 18:42:23 -0800

    Refine keywords list
    While doing filtering, I found some words in the kw (keywords) list have too many results in zenodo.
    
    For example,  'denial of service' has 1051344 results. I think this kind of "common" words are not we need.
    Thus I made the v3_kw_list, a list with the frequency in Zenodo for reference.
    
    My next step is going to use the keywords with low frequency, i.e., less than 1000.

---
+++ filterby_keywords/kw_filter.py
@@ -0,0 +1,40 @@
+import requests
+from configparser import ConfigParser
+import json
+import pandas as pd
+# Keyword Filter, to count the frequency of each keyword and get rid of the common keywords
+
+
+# Read old kw list
+kw = pd.read_csv('v2_CKIDS_keywords.csv', header=None)
+# Create the result kw list object
+kw2 = pd.DataFrame()
+
+config = ConfigParser()
+config.read('secrets.ini')
+API_ROOT = config['ZENODO_API']['API_ROOT']
+ACCESS_KEY = config['ZENODO_API']['ACCESS_TOKEN']
+
+lines = []
+for word in kw[0]:
+    params = {
+        "page": '1',
+        "size": '1',
+        'q': word,
+        'access_token': ACCESS_KEY
+    }
+    res = requests.get(
+        url=API_ROOT + 'records/', params=params)
+    # To read the count only
+    raw = res.json()
+    count = 0
+    # If hits
+    if raw.get("hits"):
+        count = raw["hits"].get("total",0)
+    line = (count, word)
+    lines.append(line)
+
+# Sort and put lines into a csv folder
+lines.sort(reverse=True)
+kw2[0] = lines
+kw2.to_csv('v3_CKIDS_keywords_with_frequency.csv')
